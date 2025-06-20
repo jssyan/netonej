923d947d388ad6b3dc21ef28a4872b4e3343a0cd
huangj <hunagj@syan.com.cn>
2025-06-20 17:02:22 +0800

    APK文件签名单元测试调整

--- src/test/java/cn/com/syan/netonej/APKFileSignDemo.java
+++ src/test/java/cn/com/syan/netonej/APKFileSignDemo.java
@@ -20,11 +20,11 @@ import java.io.RandomAccessFile;
 public class APKFileSignDemo {
 
     //设置PCS（私钥密码服务）的服务器IP与端口号
-    private PCSClient pcsClient = new PCSClient("192.168.20.223","9178");
+    private PCSClient pcsClient = new PCSClient("192.168.80.72","9178");
     //签名私钥KID
-    String kid = "86d879253d9c96bae3a688dbbdeb1186";
+    String kid = "9cd2988abf2a52b5414d5461497550c0";
     //私钥保护口令
-    String pin = "111111";
+    String pin = "Syan@9108";
 
     /**
      * 第一种方式：直接读取文件内容签名
@@ -55,49 +55,14 @@ public class APKFileSignDemo {
     @Test
     public void testFileSign_filePath() throws Exception{
         //APK文件路径
-        String in = "/Users/momocat/Documents/temp/6.apk";
+        String in = "/Users/momocat/Desktop/test/unsigned.apk";
         //输出文件路径
-        String out = "/Users/momocat/Documents/temp/7.apk";
+        String out = "/Users/momocat/Desktop/test/signed.apk";
         //签名
         pcsClient.fileSignBuilder()
-                .setId("sm2").setIdmagic(IdMagic.SCN)
+                .setId(kid)
                 .setPasswd(pin)
                 .setAlgo("SM3")
                 .build(in,out);//设置输入和输出路径
-
-        NetoneDigest digest = new NetoneDigest("md5");
-        digest.update(new FileInputStream(in));
-        System.out.println(Base64.toBase64String(digest.digest()));
-    }
-
-    @Test
-    public void testFileSignVerify_filePath() throws Exception{
-        //APK文件路径
-            String in = "/Users/momocat/Documents/temp/test_270m.apk";
-            File file = new File(in);
-            FileInputStream is = new FileInputStream(file);
-            //读取前两个字节（确定编码与内容长度）
-            byte[] bytes = new byte[2];
-            int bytesRead = is.read(bytes);
-            if (bytesRead != 2) {
-                throw new Exception("文件读取失败");
-            }
-            if(bytes[0] != 48){
-                throw new Exception("文件解析失败,非ASN1编码");
-            }
-            int lenflag = bytes[1] & 0xFF;
-            int dlen = lenflag + 2;
-            if(lenflag > 127){
-                //计算长度占多少字节
-                int byteCount = 0x7F & lenflag;
-                dlen = byteCount + 2;
-                System.out.println(dlen);
-            }
-            //开始读取版本号
-            RandomAccessFile randomAccessFile = new RandomAccessFile(file,"r");
-            randomAccessFile.seek(dlen);
-            byte[] buffer = new byte[2]; // 读取缓冲区大小
-            bytesRead = randomAccessFile.read(buffer);
-            System.out.println(Integer.toHexString(buffer[1]));
     }
 }
